80049520bdb4a112e64b5e2d4ac9fac3d6ae21ba
Sam Shoemaker <samshoemaker135@gmail.com>
2026-03-24 11:04:24 -0600

    Added some new files, created a very barebones database

---
+++ create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    username varchar(60) PRIMARY KEY,
+    password varchar(50) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS jokes (
+    id SERIAL PRIMARY KEY,
+    poster varchar(60) NOT NULL,
+    content text NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
